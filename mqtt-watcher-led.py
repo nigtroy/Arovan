7310d5d1a9420da3ca8f44db3b302d1af2337a10
OseiLecraejnr <oseidesantosjnr@gmail.com>
2018-07-14 16:34:49 +0000

    Working

--- mqtt-watcher-led.py
+++ mqtt-watcher-led.py
@@ -3,14 +3,42 @@ import time
 import uuid
 import datetime
 import json
-from pprint import pprint
 import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+from gpiozero import LED
+from time import sleep
+
+def control_led(temp):
+    if temp < 25:
+        #Cold temperatures (Blue)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(22, GPIO.OUT)
+        GPIO.output(22, True)
+        time.sleep(6)
+        GPIO.output(22, False)
+
+    elif   temp >= 25 and temp <=30 :
+	    #Normal temperatures (Green LED)	
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(27, GPIO.OUT)
+        GPIO.output(27, True)
+        time.sleep(6)
+        GPIO.output(27, False)
+    else:
+        #Very hot temperatures (Red LED)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(17, GPIO.OUT)
+        GPIO.output(17, True)
+        time.sleep(6)
+        GPIO.output(17, False)
 
 def msg_rcv(client, userdata, message):
-    # print("Received message '" + str(message.payload) + "' on topic '" + message.topic)
+    print("Received message '" + str(message.payload) + "' on topic '" + message.topic)
     try:
-        data = json.loads(str(message.payload))
-        print(data["date"])
+        data = json.loads(str(message.payload.decode("utf-8", "ignore")))
+        print(data)
+        temp = data["main"]["temp"]
+        control_led(temp)
     except:
         print("A message not intended for me, ignoring...")
 
@@ -21,8 +49,8 @@ def main_loop():
     # mosquitto_sub -h 82.165.16.151 -t UCC/mark
     client = mqtt.Client("bje_client_"+ str(uuid.UUID.hex))
     client.on_message = msg_rcv
-    # client.on_log = on_log
-    client.connect("82.165.16.151") # , port=1883 , keepalive=60, bind_address=""
+    client.on_log = on_log
+    client.connect("82.165.16.151") 
     client.loop_start()
     client.subscribe("UCC/mark")
 
