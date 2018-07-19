88131fcd9d67e6e6481f671de244570051ec73e0
samuelbartels20 <bartelssamuel20@gmail.com>
2018-07-19 10:34:53 +0100

    adding heroku files

--- jason_io.py
+++ jason_io.py
@@ -1,13 +1,12 @@
 from flask import Flask
+from flask import Flask, render_template
 import os
-
 
 app = Flask(__name__)
 
 @app.route("/")
-def output():
-	return "Hello World!"
-	
+def index():
+	return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
