4ebb5adf5ae55bae6168cf0b164fa1d84b36b84c
samuelbartels20 <bartelssamuel20@gmail.com>
2018-07-19 14:29:29 +0100

    sa.py

---
+++ sa.py
@@ -0,0 +1,9 @@
+import pyowm
+
+owm = pyowm.OWM('456a2d5e8adb346d23b30eae0b602d6f')
+observation = owm.weather_at_place('London,uk')
+w = observation.get_weather()
+
+print('Wind: '+ str(w.get_wind()))
+#w.get_humidity()
+
