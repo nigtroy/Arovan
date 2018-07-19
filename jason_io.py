a544bc2d970d68401bfb5880f270b96970d3f6df
samuelbartels20 <bartelssamuel20@gmail.com>
2018-07-19 11:25:06 +0100

    adding json files

--- jason_io.py
+++ jason_io.py
@@ -10,9 +10,13 @@ def index():
 	return render_template('index.html')
 	
 @app.route('/css/<path:path>')
-def send_js(path):
+def send_css(path):
     return send_from_directory('css', path)
-
+    
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
